41321c7451a7ef3bb6335010f33bc655e111d18f
无敌熊猫 <wusuoming@hotmail.com>
2018-09-06 20:12:21 +0800

    提取代码

--- src/main/java/com/luohuasheng/service/WaterMarkService.java
+++ src/main/java/com/luohuasheng/service/WaterMarkService.java
@@ -1,7 +1,12 @@
 package com.luohuasheng.service;
 
+import org.bytedeco.javacpp.indexer.UByteIndexer;
 import org.bytedeco.javacpp.opencv_imgproc;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
@@ -14,35 +19,38 @@ import static org.bytedeco.javacpp.opencv_imgproc.putText;
 public class WaterMarkService {
 
     private ThreadLocal<Integer> localLevel = new ThreadLocal<>();
+    private ThreadLocal<List<Integer>> localSort = new ThreadLocal<>();
+
 
     public void encode(String image, String watermark, String output, Boolean text, Integer level) {
         localLevel.set(level);
         MatVector newPlanes = new MatVector(3);
         Mat srcImg = imread(image, CV_LOAD_IMAGE_COLOR);
+        setLocalSorts(srcImg);
+        List<Integer> sorts = localSort.get();
         MatVector color = new MatVector(3);
         split(srcImg, color);
         MatVector[] planes = {new MatVector(2), new MatVector(2), new MatVector(2)};
         for (int i = 0; i < color.size(); i++) {
             color.get(i).convertTo(color.get(i), CV_32F);
             Mat comImg = startDFT(color.get(i));
-            if (level < 2) {
-                if (i == 0) {
+            if (level == 1) {
+                if (i == sorts.get(0)) {
                     if (text) {
                         addTextWaterMark(comImg, watermark);
                     } else {
                         addImageWaterMark(comImg, watermark);
                     }
                 }
-
-            } else if (level < 3) {
-                if (i < level) {
+            } else if (level == 2) {
+                if (i == sorts.get(0) || i == sorts.get(1)) {
                     if (text) {
                         addTextWaterMark(comImg, watermark);
                     } else {
                         addImageWaterMark(comImg, watermark);
                     }
                 }
-            } else {
+            } else if (level == 3) {
 
                 if (text) {
                     addTextWaterMark(comImg, watermark);
@@ -59,6 +67,32 @@ public class WaterMarkService {
         merge(newPlanes, nImg);
         imwrite(output, nImg);
         localLevel.remove();
+        localSort.remove();
+    }
+
+    public void setLocalSorts(Mat srcImg) {
+        UByteIndexer indexer = srcImg.createIndexer();
+        List<PixelSum> pixelSums = new ArrayList<>();
+        List<Integer> sorts = new ArrayList<>();
+        //图像通道
+        int nbChannels = srcImg.channels();
+        for (int channel = 0; channel < nbChannels; channel++) {
+            int channelSum = 0;
+            for (int i = 0; i < srcImg.rows(); i++) {
+                for (int j = 0; j < srcImg.cols(); j++) {
+                    channelSum += (indexer.get(i, j, channel));
+                }
+            }
+            PixelSum pixelSum = new PixelSum();
+            pixelSum.setIndex(channel);
+            pixelSum.setSum(channelSum);
+            pixelSums.add(pixelSum);
+        }
+        pixelSums.sort(Comparator.comparing(PixelSum::getSum));
+        for (PixelSum pixelSum : pixelSums) {
+            sorts.add(pixelSum.getIndex());
+        }
+        localSort.set(sorts);
     }
 
     /**
@@ -69,13 +103,23 @@ public class WaterMarkService {
      */
     public void decode(String wmImg, String output, Integer level) {
         localLevel.set(level);
-        if (level < 3) {
-            Mat srcImg = imread(wmImg, CV_LOAD_IMAGE_COLOR);
-            imwrite(output, transformImage(srcImg));
-        } else {
-            Mat srcImg = imread(wmImg, CV_LOAD_IMAGE_GRAYSCALE);
-            imwrite(output, transformImage2(srcImg));
+
+        MatVector newPlanes = new MatVector(3);
+
+        for (int i = 0; i < 3; i++) {
+            Mat srcImg = imread(wmImg, i == 2 ? CV_LOAD_IMAGE_GRAYSCALE : CV_LOAD_IMAGE_COLOR);
+            if (localSort.get() == null) {
+                setLocalSorts(srcImg);
+            }
+            localLevel.set(i + 1);
+            newPlanes.put(i, transformImage(srcImg));
         }
+        Mat nImg2 = new Mat();
+        Mat nImg = new Mat();
+        vconcat(newPlanes.get(0), newPlanes.get(1), nImg);
+        vconcat(nImg, newPlanes.get(2), nImg2);
+//        merge(newPlanes,nImg2);
+        imwrite(output, nImg2);
         localLevel.remove();
     }
 
@@ -184,7 +228,6 @@ public class WaterMarkService {
 
         Scalar scalar = new Scalar(255, 255, 255, 255);
         Point p = new Point(40, 40);
-
         // 添加字符串
         putText(comImg, watermark, p, opencv_imgproc.CV_FONT_HERSHEY_DUPLEX, 1, scalar, 2, LINE_8, false);
         // 旋转图片
@@ -236,18 +279,22 @@ public class WaterMarkService {
     public Mat transformImage(Mat image) {
         MatVector planes = new MatVector(2);
         Mat complexImage = new Mat();
-        Integer integer = localLevel.get();
-        if (integer < 2) {
-            Mat padded = splitSrc(image, 0);
+        Integer level = localLevel.get();
+        List<Integer> sorts = localSort.get();
+
+        if (level == 1) {
+            Mat padded = splitSrc(image, sorts.get(0));
             padded.convertTo(padded, CV_32F);
             planes.put(0, padded);
             planes.put(1, padded);
-        } else {
+        } else if (level == 2) {
             for (int i = 0; i < 2; i++) {
-                Mat padded = splitSrc(image, i);
+                Mat padded = splitSrc(image, sorts.get(i));
                 padded.convertTo(padded, CV_32F);
                 planes.put(i, padded);
             }
+        } else {
+            return transformImage2(image);
         }
         merge(planes, complexImage);
         dft(complexImage, complexImage);
@@ -286,6 +333,27 @@ public class WaterMarkService {
             return color.get(index);
         } else {
             return mat;
+        }
+    }
+
+    private class PixelSum {
+        private Integer index;
+        private Integer sum;
+
+        public Integer getIndex() {
+            return index;
+        }
+
+        public void setIndex(Integer index) {
+            this.index = index;
+        }
+
+        public Integer getSum() {
+            return sum;
+        }
+
+        public void setSum(Integer sum) {
+            this.sum = sum;
         }
     }
 
